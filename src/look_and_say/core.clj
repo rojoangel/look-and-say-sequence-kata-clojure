3654b7ccc8d9536937aba4b3e9bbe0604b7baa5b
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-07-08 21:29:40 +0200

    calling next-n for one digit returns one and the digit

--- src/look_and_say/core.clj
+++ src/look_and_say/core.clj
@@ -1,1 +1,4 @@
 (ns look-and-say.core)
+
+(defn next-n [n]
+  (+ 10 n))
--- test/look_and_say/core_test.clj
+++ test/look_and_say/core_test.clj
@@ -1,3 +1,8 @@
 (ns look-and-say.core-test
   (:require [clojure.test :refer :all]
             [look-and-say.core :refer :all]))
+
+(deftest test-next-n
+  (testing "calling next-n for one digit returns one and the digit"
+    (is (= 11 (next-n 1)))
+    (is (= 19 (next-n 9)))))
