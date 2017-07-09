332bca0d6e5dbc33b2e54748abcbd5cae668dfcd
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-07-09 11:05:10 +0200

    refactor 'count-me-in' to deal with the acc via conj instead of concat - perf improvement? -> produces a result that needs to be reversed

--- src/look_and_say/core.clj
+++ src/look_and_say/core.clj
@@ -2,14 +2,15 @@
   (:require [look-and-say.aux :as aux]))
 
 (defn- count-me-in [acc n]
-  (let [last-count (last (butlast acc))
-        last-n (last acc)]
+  (let [last-count (second acc)
+        last-n (first acc)]
     (if (= n last-n)
-      (concat (drop-last 2 acc) [(inc last-count) n])
-      (concat acc [1 n]))))
+      (conj (drop 2 acc) (inc last-count) n)
+      (conj acc 1 n))))
 
 (defn next-n [n]
   (->> n
        aux/n->seq
        (reduce count-me-in nil)
+       reverse
        aux/seq->n))
